0a3d292f336831c2adc9424a7a0343c77c85f651
David Carlin <davich@gmail.com>
2018-06-02 15:03:32 +1000

    Move to a class method

--- sortby.rb
+++ sortby.rb
@@ -1,28 +1,35 @@
 class Sortby
-  def initialize
-    @first = {}
-    @second = {}
+  def self.sort_by(array, first, second)
+    new(array, first, second).sort
   end
 
-  def sort(array, first, second)
-    array.sort do |left, right|
-      x = cache_first(first, left) <=> cache_first(first, right)
+  def initialize(array, first, second)
+    @array = array
+    @first = first
+    @second = second
+    @first_cache = {}
+    @second_cache = {}
+  end
+
+  def sort
+    @array.sort do |left, right|
+      x = cache_first(left) <=> cache_first(right)
       if x == 0
-        cache_second(second, left) <=> cache_second(second, right)
+        cache_second(left) <=> cache_second(right)
       else
         x
       end
     end
   end
 
-  def cache_first(proc, elem)
-    @first[elem] ||= proc.(elem)
+  def cache_first(elem)
+    @first_cache[elem] ||= @first.(elem)
   end
 
-  def cache_second(proc, elem)
-    @second[elem] ||= proc.(elem)
+  def cache_second(elem)
+    @second_cache[elem] ||= @second.(elem)
   end
 end
 
 names = [['erlangga', 'last'], ['david', 'carlin'],['david', 'abc'],['david', 'rider'], ['angel', 'something']]
-puts Sortby.new.sort(names, ->(x){puts x.first ; x.first}, ->(x){puts x.last ; x.last}).inspect
+puts Sortby.sort_by(names, ->(x){puts x.first ; x.first}, ->(x){puts x.last ; x.last}).inspect
